edfc80cfbaae4e867a22aac42c785eb8fb531cc6
basaktuysuz1 <basak_5363@hotmail.com>
2025-01-27 15:57:43 +0300

    comm2

---
+++ u4_ikisayı_topla/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("1. sayıyı gir: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. sayıyı gir: ");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.Write(a+b);
---
+++ u6_alanveçevre/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Dörtgenin a kenarını girin : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Dörtgenin b kenarını girin : ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+var cevre = (a+b)*2;
+var alan = (a*b);
+
+Console.Write("Dikdörtgenin kenarları ");
+Console.WriteLine(string.Format("{0}cm  ve {1}cm  ", a, b));
+
+Console.WriteLine($"Kenarları {a}cm  ve {b}cm  olan Dik dörtgenin alanı {a} * {b} = {alan}'cm dir");//string interpolation
+
+
+Console.WriteLine($"Kenarları {a}cm  ve {b}cm  olanDikdörtgenin Çevresi {cevre}'cm dir");//string interpolation
